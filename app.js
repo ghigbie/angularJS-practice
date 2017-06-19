77961bd00ca1638f2d564c66a23c23b54774da14
George Higbie <gehigbie@gmail.com>
2017-06-19 15:11:26 +0000

    Add new form-control item

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 /* global angular*/
 /* global app*/
 
-let app = angular.module("minmax", []);
+var app = angular.module("minmax", []);
 
-app.controller("MinMaxCtrl", function($scope){
-    $scope.formModel = {};
-});
 
