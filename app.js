2be98148c36ede589c84e8176fe756d5062d5521
George Higbie <gehigbie@gmail.com>
2017-06-19 15:46:30 +0000

    Modify form

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 /* global angular*/
 /* global app*/
-(function() {
-    let app = angular.model("minmax", []);
-}());
 
+// (function() {
+//     let app = angular.model("minmax", []);
+// }());
+
+app.controller("MinMaxCtrl", function($scope, $http){
+    $scope.formModel = {};
+    
+    $scope.onSubmit = function() {
+        console.log("Hey i'm submitted");
+        console.log($scope.formModel);
+        
+        $http.post('https://minmax-server.herokuapp.com/register', $scope.formModel)
+            .success(function(data){
+                console.log(": )");
+            }).error(function(Data){
+               console.log(": ("); 
+            });
+    };
+});
